3ef4db482f77a111d8d1653fe21ac867966f3588
niha-2 <gen.3polym.hn@gmail.com>
2025-01-21 09:03:04 +0900

    [add] MyBatis update, delete-1

--- src/main/kotlin/com/example/demo/Main.kt
+++ src/main/kotlin/com/example/demo/Main.kt
@@ -2,7 +2,9 @@ package com.example.demo
 
 import com.example.demo.database.*
 import com.example.demo.database.UserDynamicSqlSupport.User.age
+import com.example.demo.database.UserDynamicSqlSupport.User.id
 import com.example.demo.database.UserDynamicSqlSupport.User.name
+import com.example.demo.database.UserDynamicSqlSupport.User.profile
 import org.apache.ibatis.io.Resources
 import org.apache.ibatis.session.SqlSessionFactory
 import org.apache.ibatis.session.SqlSessionFactoryBuilder
@@ -10,7 +12,7 @@ import org.mybatis.dynamic.sql.SqlBuilder.isEqualTo
 import org.mybatis.dynamic.sql.SqlBuilder.isGreaterThanOrEqualTo
 
 fun main() {
-    list5_4_12()
+    list5_4_20()
 }
 fun createSessionFactory(): SqlSessionFactory {
     val resource = "mybatis-config.xml"
@@ -18,6 +20,7 @@ fun createSessionFactory(): SqlSessionFactory {
     return SqlSessionFactoryBuilder().build(inputStream)
 }
 
+//select
 fun list5_4_3() {
     createSessionFactory().openSession().use { session ->
         val mapper = session.getMapper(UserMapper::class.java)
@@ -65,6 +68,7 @@ fun list5_4_9() {
     }
 }
 
+// insert
 fun list5_4_10() {
     val user = UserRecord(103, "Shiro", 18, "Hello")
     createSessionFactory().openSession().use { session ->
@@ -82,5 +86,54 @@ fun list5_4_12() {
         val count = mapper.insertMultiple(userList)
         session.commit()
         println("${count}行のレコードを挿入しました")
+    }
+}
+
+// update
+// 主キー
+fun list5_4_14() {
+    val user = UserRecord(id = 105, profile = "Bye")
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.updateByPrimaryKeySelective(user)
+        session.commit()
+        println("${count}行のレコードを更新しました")
+    }
+}
+
+// 主キー以外のカラムを検索条件としたレコード更新(Recordオブジェクトを使わない場合)
+fun list5_4_16() {
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.update {
+            set(profile).equalTo("Hey")
+            where(id, isEqualTo(104))
+        }
+        session.commit()
+        println("${count}行のレコードを更新しました")
+    }
+}
+
+// 主キー以外のカラムを検索条件としたレコード更新(Recordオブジェクトを使う場合)
+fun list5_4_18() {
+    val user = UserRecord(profile = "Good Morning")
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.update {
+            updateSelectiveColumns(user)
+            where(name, isEqualTo("Shiro"))
+        }
+        session.commit()
+        println("${count}行のレコードを更新しました")
+    }
+}
+
+// delete
+fun list5_4_20() {
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.deleteByPrimaryKey(102)
+        session.commit()
+        println("${count}行のレコードを削除しました")
     }
 }
